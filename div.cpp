2e80655b21c57ee7890a66cba809f69da163805f
Aviral Kumar <aviralkumar1904@gmail.com>
2023-05-31 14:39:20 +0530

    commit all

---
+++ div.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a=6;
+    int b=3;
+
+    cout<<a/b;
+    return 0;
+}
---
+++ mutip.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a=4,b=3;
+
+    cout<<a*b;
+    return 0;
+}
